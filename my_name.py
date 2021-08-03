edf6931a2733948d567222fb117ccb81601b0752
levels0230 <levels0230@gmail.com>
2021-08-03 14:09:17 +0800

    Add function to print stars

--- my_name.py
+++ my_name.py
@@ -1,3 +1,12 @@
 """
 Bingkun Li
 """
+
+def main():
+    """Start the program"""
+    print("Hello, My name is Bingkun Li")
+    for i in range(1, 6):
+        print_stars(i)
+
+def print_stars(number):
+    print("*"* number)
